4ea92be359d2da681f47a7e8696993e76a7f99e2
ankitpakhale <akp3067@gmail.com>
2022-03-15 12:31:59 +0530

    auth frontend commit

--- clientWebsocket/clientapp/views.py
+++ clientWebsocket/clientapp/views.py
@@ -1,13 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponse
+from . models import *
 # Create your views here.
 
 def CTest(request):
     return HttpResponse("on clientWebsocket")
 
-def Signup(request):
-    return HttpResponse("on Signup view")
+def Signup(self):
+    if self.POST:
+        Name = self.POST['name']
+        print(Name)
+        Email = self.POST['email']
+        print(Email)
+        Password = self.POST['password']
+        print(Password)
+        ConfirmPassword = self.POST['confirmPassword']
+        print(ConfirmPassword)
 
-def Login(request):
-    return HttpResponse("on Login View")
+        try:
+            data=clientuser.objects.filter(email=Email)
+            if data:
+                msg = 'Email already taken'
+                return render(self , 'signup.html',{'msg':msg})
+
+            elif ConfirmPassword == Password:
+                v = clientuser()
+                v.name = Name
+                v.email = Email
+                v.password = Password
+                v.save()
+                return redirect('LOGIN')
+
+            else:
+                msg = 'Enter Same Password'
+                return render(self , 'signup.html',{'msg':msg}) 
+                
+        finally:
+            messages.success(self, 'Signup Successfully Done...')
+
+    return render(self,'signup.html')
+
+def Login(self):
+    if self.POST:
+        em = self.POST.get('email')
+        pass1 = self.POST.get('password')
+        try:
+            print("Inside first try block")
+            check = clientuser.objects.get(email = em)
+            print("Email is ",em,check.email)
+            if check.password == pass1:
+                # print(check.Password)
+                self.session['email'] = check.email
+                return HttpResponse('Signed up successfully')
+                # return redirect('CADASHBOARD')
+
+                # nameMsg = CasignUp.objects.get(email = em)
+                # msg = 'User Successfully logged in'
+                # print(msg)
+                # return render(self, 'dashboard.html', {'key':nameMsg})
+            else:
+                return HttpResponse('Invalid Password')
+        except:
+            print("Inside first except block")
+            return HttpResponse('Invalid Email ID')
+    return render(self,'login.html')
+
+
+
 
