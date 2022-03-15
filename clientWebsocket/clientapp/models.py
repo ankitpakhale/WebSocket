ea86e92a26c85c6cc971936781f4c81b4b7e3d1d
ankitpakhale <akp3067@gmail.com>
2022-03-15 12:07:37 +0530

    migrations commit

--- clientWebsocket/clientapp/models.py
+++ clientWebsocket/clientapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class clientuser(models.Model):
+    name=models.CharField(default='',max_length=45)
+    email=models.EmailField()
+    password=models.CharField(default='',max_length=45)
+    def __str__(self) -> str:
+        return self.name
