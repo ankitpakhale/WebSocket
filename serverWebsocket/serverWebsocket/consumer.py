596edf3f01ad0d6a22e7bb730c68907dd80ff5c6
ankitpakhale <akp3067@gmail.com>
2022-03-16 09:16:08 +0530

    socket commit

--- serverWebsocket/serverWebsocket/consumer.py
+++ serverWebsocket/serverWebsocket/consumer.py
@@ -11,10 +11,12 @@ class ChatConsumer(WebsocketConsumer):
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        print(f'message come from client {message}')
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        print(text_data_json)
+
+        # message = text_data_json['message']
+        # print(f'message come from client {message}')
+        # self.send(text_data=json.dumps({
+        #     'message': message
+        # }))
     # def send(self, text_data=None, bytes_data=None, close=False):
     #     return super().send(text_data, bytes_data, close)()
